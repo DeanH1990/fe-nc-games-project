14bb9effdd72f34ae88741ef188713f367a518f1
DeanH1990 <dhasley1990@gmail.com>
2022-11-03 09:06:05 +0000

    optimistically render votes

--- src/components/UpdateVotes.jsx
+++ src/components/UpdateVotes.jsx
@@ -6,10 +6,10 @@ const UpdateVotes = (props) => {
     const [error, setError] = useState(null);
 
     const [increaseisClicked, setIncreaseIsClicked] = useState(false);
-    const [increaseVotes, setIncreaseVotes] = useState(0);
+    const [increaseVotes, setIncreaseVotes] = useState(votes);
 
     const [decreaseisClicked, setdecreaseIsClicked] = useState(false);
-    const [decreaseVotes, setDecreaseVotes] = useState(1);
+    const [decreaseVotes, setDecreaseVotes] = useState(0);
 
     const handleIncreaseVotes = () => {
         setIncreaseVotes((curVotes) => curVotes + 1)
@@ -28,7 +28,6 @@ const UpdateVotes = (props) => {
         setdecreaseIsClicked(true);
         setIncreaseIsClicked(false);
         api.updateReviewVotesById(review_id, -1).then((data) => {
-           
         }).catch((err) => {
             setdecreaseIsClicked(false);
             setDecreaseVotes((curVotes) => curVotes + 1)
@@ -38,7 +37,7 @@ const UpdateVotes = (props) => {
 
     return error ? <h3>Something went wrong, please try again</h3> : 
     <>
-        <p>Votes: {votes } </p>
+        <p>Votes: {increaseVotes + decreaseVotes} </p>
         <div className='vote-btn-container'>
             <button className='inc-vote-btn' disabled={increaseisClicked} onClick={handleIncreaseVotes}>{'\u{2B06}'}</button>
             <button className='inc-vote-btn' disabled={decreaseisClicked} onClick={handleDecreaseVotes}>{'\u{2B07}'}</button>
